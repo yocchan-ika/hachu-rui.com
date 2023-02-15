cbb67186f460d0608ec8ec49c46e1294ba9f0222
ysk0115 <125228853+ysk0115@users.noreply.github.com>
2023-02-15 13:44:13 +0900

    Amazon[SP-API]のファイルを新規Up

---
+++ Amazon[SP-API].py
@@ -0,0 +1,9 @@
+credentials=dict(
+            refresh_token     = 'リフレッシュトークン',   # Amazon Seller開発者登録後に入手可能
+            lwa_app_id        = 'クライアントID',        # Amazon Seller開発者登録後に入手可能
+            lwa_client_secret = 'クライアント機密情報',   # Amazon Seller開発者登録後に入手可能
+            aws_access_key    = 'AWS アクセスキー',      #（AWS IAMユーザーロール登録時に取得可能）
+            aws_secret_key    = 'AWS シークレットキー',  #（AWS IAMユーザーロール登録時に取得可能）
+            role_arn          = 'AWS IAM ARN',        #（AWS IAMユーザーロール登録時に取得可能）
+            )
+
